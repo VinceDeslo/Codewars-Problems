c72fb2338cf44ba302242f0e552e797bcdf7a3c7
VinceDeslo <v.desloover2@gmail.com>
2021-01-23 10:03:38 -0500

    Cofactor function works

--- MatrixDeterminant/matrix-determinant.ts
+++ MatrixDeterminant/matrix-determinant.ts
@@ -4,28 +4,51 @@ export function determinant(matrix: number[][]) {
     let res: number = 0;
     let sign: number = -1;
 
-    // Iterate over the first row
-    for(let i=0; i<matrix[0].length; i++){
-        
-        // Calculate the minor determinant
-        let exp: number = 0;
+    // 2x2 guard
+    if(matrix[0].length === 2) return det(matrix);
+
+    // Iterate over the columns
+    for(let i=0; i < matrix[0].length; i++){
 
         // Multiply by the sign according to position
-        if(i % 2) res += exp*sign;
+        if(i % 2) sign*=-1;
+
+        // Calculate the minor determinant (cofactor)
+        res += sign*getCofact(matrix, 0, i);
     }
 
     // Default return (2x2)
-    return det(matrix);
+    return res;
 }
 
 // Simple determinant
 function det(matrix: number[][]){
-    
-    const ad = matrix[0][0] * matrix[1][1];
-    const bc = matrix[0][1] * matrix[1][0];
-    return ad - bc;
+
+    console.log('determinant of: ' + matrix)
+    return matrix[0][0] * matrix[1][1] -matrix[0][1] * matrix[1][0];
+}
+
+// Get cofactor according to position in matrix
+function getCofact(matrix: number[][], rowNum: number, colNum: number) : number{
+
+    console.log('matrix= ' + matrix)
+    console.log('row: '+rowNum+ ' col: '+colNum)
+    let copy = [...matrix];
+
+    // Remove the given row
+    copy.splice(rowNum, 1)
+
+    // Remove the given column
+    copy.map((val)=>{
+        console.log('val= '+ val)
+        val.splice(colNum, 1)
+    })
+
+    console.log('submatrix= ' +copy);
+
+    return det(copy);
 }
 
 // Call the function
-console.log(determinant([[1,3], [2,5]]));
+console.log(determinant([[1,3,4], [2,5,7], [4,6,8]]));
   
